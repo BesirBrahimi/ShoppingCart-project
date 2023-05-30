59ad32bd994fbab3aaffb022c304f76bbae3dc7b
Besir Brahimi <besirbrahimi7@gmail.com>
2023-05-30 20:08:18 +0200

    fixed some details

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import "./Navbar.css";
+import useCartContext from "../useCartContext";
 
 const Navbar = () => {
+  const { boughtProducts } = useCartContext();
   const location = useLocation();
 
   const isNavLinkActive = (path: string) => {
     return location.pathname === path;
+  };
+
+  const getProductQuantity = () => {
+    return boughtProducts.reduce((total, product) => total + product.quantity, 0);
   };
 
   return (
@@ -29,6 +35,11 @@ const Navbar = () => {
             }`}
           >
             Bought Products
+            {boughtProducts.length > 0 && (
+              <span className="boughtProducts-length">
+                {getProductQuantity()}
+              </span>
+            )}
           </NavLink>
         </li>
       </ul>
